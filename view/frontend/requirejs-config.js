574692cfe51002e39285a15428acfbd6bfaf55e0
Bezigeboefjes <git@bezigeboefjes.nl>
2017-10-19 14:25:42 +0200

    Integrated owl-carousel

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    paths: {
+        owlcarousel: 'BezigeBoefjes_NewProductCarouselWidget/js/owl.carousel.min'
+    },
+    "shim": {
+        owlcarousel: {
+             deps: ['jquery']
+        }
+    }
+};
+
